2575fdd30d01cc6b231c8a2e4614129cbbc66d74
Amir <am17iryaghoobi@gmail.com>
2025-02-21 01:13:27 +0330

    feat(users): define User model with basic fields

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -131,3 +131,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Media files (Uploaded files)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+# Custom user model
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    about_me = models.TextField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_image = models.ImageField(upload_to="users/profile-images/")
+
+    def __str__(self):
+        return self.username
