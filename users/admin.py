0519ac744a49af20745a05f032ee2150118a8878
Amir <am17iryaghoobi@gmail.com>
2025-02-21 01:34:45 +0330

    feat(users): add User model to Django admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ["username", "first_name", "last_name", "email"]
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional Info",
+            {"fields": ("about_me", "date_of_birth", "profile_image")},
+        ),
+    )
