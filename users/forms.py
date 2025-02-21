62e5e8aba9bbbc2c4965e5a588fa80a14672460c
Amir <am17iryaghoobi@gmail.com>
2025-02-21 15:28:56 +0330

    feat(forms): improve validation and structure of registration form

--- users/forms.py
+++ users/forms.py
@@ -1,61 +1,39 @@
 from django import forms
 from .models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegistrationForm(forms.ModelForm):
-    confirm_password = forms.CharField(
-        max_length=16,
-        widget=forms.PasswordInput(),
-        required=True,
-        label="Confirm password",
-    )
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Email")
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "confirm_password"]
-
-    def clean_confirm_password(self):
-        cd = self.cleaned_data
-        password = cd.get("password")
-        confirm_password = cd.get("confirm_password")
-
-        if not password or not confirm_password:
-            raise forms.ValidationError("Password and Confirm password are required!")
-
-        if password != confirm_password:
-            raise forms.ValidationError(
-                "The password and its confirmation do not match."
-            )
-
-        return confirm_password
+        fields = ["username", "email", "password1", "password2"]
+        widgets = {
+            "password1": forms.PasswordInput(),
+            "password2": forms.PasswordInput(),
+        }
 
     def clean_username(self):
-        cd = self.cleaned_data
-        username = cd.get("username")
+        username = self.cleaned_data.get("username")
 
-        # Check if the username is provided
         if not username:
-            raise forms.ValidationError("username is required!")
+            raise forms.ValidationError("Username is required!")
 
-        # Check if the username is at least 4 characters long
         if len(username) < 4:
             raise forms.ValidationError(
                 "The username must be at least 4 characters long!"
             )
 
-        # Check if the username already exists in the database
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError("This username already exists!")
 
         return username
 
     def clean_email(self):
-        cd = self.cleaned_data
-        email = cd.get("email")
+        email = self.cleaned_data.get("email")
 
-        # If an email is provided, check if it already exists in the database
-        if email:
-            if User.objects.filter(email=email).exists():
-                raise forms.ValidationError("This email already exists!")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email already exists!")
 
         return email
