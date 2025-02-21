cc508d7337cb297d0befb9b217be50f33d654ea2
Amir <am17iryaghoobi@gmail.com>
2025-02-21 15:50:10 +0330

    feat(users): add login class and template for test

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path("register/", views.UserRegisterationView.as_view(), name="register")
+    path("register/", views.UserRegisterationView.as_view(), name="register"),
+    path("login/", views.Login.as_view(), name="login"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import User
 from .forms import UserRegistrationForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.urls import reverse_lazy
 
 
@@ -9,4 +9,8 @@ class UserRegisterationView(CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = "forms/signup.html"
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("users:login")
+
+
+class Login(TemplateView):
+    template_name = "forms/login.html"
