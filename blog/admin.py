d8a8f9ead572fe19e75334c2ed3bc17633babefc
Amir <am17iryaghoobi@gmail.com>
2025-03-03 00:17:23 +0330

    feat(blog): Create Category, Post and PostImage models.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Post, PostImage
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ["name"]}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "author", "created_at"]
+    list_filter = ["category", "author"]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [models.Index(fields=["name"])]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name="posts", null=True
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["author"]),
+            models.Index(fields=["category"]),
+            models.Index(fields=["-created_at"]),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_images")
+    image = models.ImageField(upload_to="posts/images/")
+
+    def __str__(self):
+        return f"image for : {self.post}"
