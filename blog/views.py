70e0b0098966c03e7c81c18ec73f752599dfcf11
Amir <am17iryaghoobi@gmail.com>
2025-03-05 22:54:10 +0330

    style(blog): improve styling and layout of post details page

--- blog/views.py
+++ blog/views.py
@@ -39,8 +39,18 @@ class PostListView(ListView):
     template_name = "blog/post_list.html"
     context_object_name = "posts"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+
 
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
     context_object_name = "post"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
