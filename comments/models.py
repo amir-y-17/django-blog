7fea7f8f06b4f89520c20ae68119063d92b4e345
Amir <am17iryaghoobi@gmail.com>
2025-03-08 16:41:31 +0330

    create comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import User
+from blog.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [models.Index(fields=["-created_at"])]
+
+    def __str__(self):
+        return self.content
