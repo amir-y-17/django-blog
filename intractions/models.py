96d89f9b3c34cd23f81df0c1787a8b5c9cc9e4da
Amir <am17iryaghoobi@gmail.com>
2025-03-10 15:04:21 +0330

    create Like model and add to admin

--- intractions/models.py
+++ intractions/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from blog.models import Post
+from users.models import User
 
-# Create your models here.
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date"]
+        indexes = [models.Index(fields=["-date"])]
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"
