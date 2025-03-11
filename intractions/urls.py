267286a7fd712cd4cc423180821e673189976f6f
Amir <am17iryaghoobi@gmail.com>
2025-03-11 15:28:59 +0330

    feat(intractions): add save post feature

--- intractions/urls.py
+++ intractions/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "intractions"
 
 urlpatterns = [
-    path("Like/post-<int:pk>/", views.LikePostView.as_view(), name="like_post")
+    path("Like/post-<int:pk>/", views.LikePostView.as_view(), name="like_post"),
+    path("save/post-<int:pk>/", views.SavePostView.as_view(), name="save_post"),
 ]
--- intractions/views.py
+++ intractions/views.py
@@ -1,6 +1,5 @@
-from .models import Like
+from .models import Like, SavedPost
 from blog.models import Post
-from users.models import User
 from django.http import JsonResponse
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
@@ -26,3 +25,20 @@ class LikePostView(LoginRequiredMixin, View):
         post_likes_count = post.likes.count()
         response_data = {"liked": liked, "likes_count": post_likes_count}
         return JsonResponse(response_data)
+
+
+class SavePostView(LoginRequiredMixin, View):
+    login_url = "users:login"
+
+    def post(self, request, **kwargs):
+        post = get_object_or_404(Post, id=kwargs.get("pk"))
+        user = request.user
+
+        saved_post, created = SavedPost.objects.get_or_create(post=post, user=user)
+        if not created:
+            saved_post.delete()
+            saved = False
+        else:
+            saved = True
+
+        return JsonResponse({"saved": saved})
